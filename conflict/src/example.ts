40eef520f6167716ac521aabf16e05ce9699046b
Mike Auclair <mike@mikeauclair.com>
2018-10-26 17:30:42 -0500

    prettier

--- conflict/src/example.ts
+++ conflict/src/example.ts
@@ -1,28 +1,45 @@
-import {Person, Vehicle, LegacyInput, Exclusion, Group, Output} from '../../shared/edge_types'
+import {
+  Person,
+  Vehicle,
+  LegacyInput,
+  Exclusion,
+  Group,
+  Output
+} from "../../shared/edge_types";
 
-import {payload as legacyInputInitial} from '../../shared/legacy_input_payloads/initial.js';
-import {payload as legacyInputMissingPersonOne} from '../../shared/legacy_input_payloads/missing_person_one.js';
-import {payload as legacyInputMissingPersonOneAndTwo} from '../../shared/legacy_input_payloads/missing_person_one_and_two.js';
+import { payload as legacyInputInitial } from "../../shared/legacy_input_payloads/initial.js";
+import { payload as legacyInputMissingPersonOne } from "../../shared/legacy_input_payloads/missing_person_one.js";
+import { payload as legacyInputMissingPersonOneAndTwo } from "../../shared/legacy_input_payloads/missing_person_one_and_two.js";
 
 type Command = "set" | "remove" | "noop";
-type TargetType = 'exclusion' | 'group' | 'groupLimit' | 'vehicleGroupAssignment' | "none";
+type TargetType =
+  | "exclusion"
+  | "group"
+  | "groupLimit"
+  | "vehicleGroupAssignment"
+  | "none";
 type Target = number;
 // Really this could be number | number[] | event, but you run into having to add interface indirection
 // see: https://github.com/Microsoft/TypeScript/issues/3496#issuecomment-128553540
 type Value = any;
 
 type Message = {
-  level: "NOTIFY" | "WARN" | "ERROR"
-  content: string
-}
+  level: "NOTIFY" | "WARN" | "ERROR";
+  content: string;
+};
 
 interface HandlerResponse {
-  output: Output
-  message?: Message
-  event: event
+  output: Output;
+  message?: Message;
+  event: event;
 }
 
-enum EventIndex {Command, Type, Target, Value};
+enum EventIndex {
+  Command,
+  Type,
+  Target,
+  Value
+}
 
 type event = [Command, TargetType, Target, Value];
 
@@ -45,35 +62,47 @@ function assertNever(x: never, label: string): never {
 let noopFor = (e: event) => {
   let output: event = ["noop", "none", 0, e];
   return output;
-}
+};
 
 interface DriverLookupResult {
-  found: Person[]
-  missing: number[]
+  found: Person[];
+  missing: number[];
 }
 
-let handleSet = (legacyInput: LegacyInput, e: event, output: Output): HandlerResponse => {
+let handleSet = (
+  legacyInput: LegacyInput,
+  e: event,
+  output: Output
+): HandlerResponse => {
   let eventType = e[EventIndex.Type];
   switch (eventType) {
     case "exclusion":
       let driver = legacyInput.people.find(p => p.id === e[EventIndex.Target]);
-      let vehicle = legacyInput.vehicles.find(v => v.id === e[EventIndex.Value]);
-      if (!driver || !vehicle ){
-        return {output: output, event: noopFor(e), message: {level: "NOTIFY", content: "Exclusion Removed"}};
+      let vehicle = legacyInput.vehicles.find(
+        v => v.id === e[EventIndex.Value]
+      );
+      if (!driver || !vehicle) {
+        return {
+          output: output,
+          event: noopFor(e),
+          message: { level: "NOTIFY", content: "Exclusion Removed" }
+        };
       }
       output.exclusions.push({ driver_id: driver.id, vehicle_id: vehicle.id });
-      return {output: output, event: e};
+      return { output: output, event: e };
     case "group":
-      let drivers = e[EventIndex.Value].reduce((accum: DriverLookupResult, pid: number) => {
-        let driver = legacyInput.people.find(p => p.id === pid);
-        if (driver) {
-          accum.found.push(driver);
-        } else {
-          accum.missing.push(pid);
-        }
-        return accum;
-          
-      }, {found: [], missing: []});
+      let drivers = e[EventIndex.Value].reduce(
+        (accum: DriverLookupResult, pid: number) => {
+          let driver = legacyInput.people.find(p => p.id === pid);
+          if (driver) {
+            accum.found.push(driver);
+          } else {
+            accum.missing.push(pid);
+          }
+          return accum;
+        },
+        { found: [], missing: [] }
+      );
       if (drivers.found.length) {
         output.groups.push({
           id: e[EventIndex.Target],
@@ -81,60 +110,107 @@ let handleSet = (legacyInput: LegacyInput, e: event, output: Output): HandlerRes
           drivers: drivers.found
         });
         if (drivers.missing.length) {
-          let newEvent: event = ["set", "group", e[EventIndex.Target], drivers.found.map((d: any) => d.id)];
-          return {output: output, event: newEvent, message: {level: "WARN", content: "Some drivers were removed from this group"}};
+          let newEvent: event = [
+            "set",
+            "group",
+            e[EventIndex.Target],
+            drivers.found.map((d: any) => d.id)
+          ];
+          return {
+            output: output,
+            event: newEvent,
+            message: {
+              level: "WARN",
+              content: "Some drivers were removed from this group"
+            }
+          };
         } else {
-          return {output: output, event: e}
+          return { output: output, event: e };
         }
       } else {
-        return {output: output, event: noopFor(e), message: {level: "WARN", content: "This group is invalid because all drivers have been removed"}};
+        return {
+          output: output,
+          event: noopFor(e),
+          message: {
+            level: "WARN",
+            content:
+              "This group is invalid because all drivers have been removed"
+          }
+        };
       }
       break;
     case "groupLimit":
       var group = output.groups.find(g => g.id === e[EventIndex.Target]);
       if (group) {
         group.limit = e[EventIndex.Value];
-        return {output: output, event: e}
+        return { output: output, event: e };
       } else {
-        return {output: output, event: noopFor(e), message: {level: "NOTIFY", content: "Limit removed"}};
+        return {
+          output: output,
+          event: noopFor(e),
+          message: { level: "NOTIFY", content: "Limit removed" }
+        };
       }
       break;
     case "vehicleGroupAssignment":
-      var group = output.groups.find(g => g.id === e[EventIndex.Target])
+      var group = output.groups.find(g => g.id === e[EventIndex.Target]);
       if (group) {
-        let vehicle = legacyInput.vehicles.find(v => v.id === e[EventIndex.Value]);
+        let vehicle = legacyInput.vehicles.find(
+          v => v.id === e[EventIndex.Value]
+        );
         if (vehicle) {
           group.vehicles.push(vehicle);
-          return {output: output, event: e}
+          return { output: output, event: e };
         } else {
-          return {output: output, event: noopFor(e), message: {level: "WARN", content: "Vehicle has been removed"}};
+          return {
+            output: output,
+            event: noopFor(e),
+            message: { level: "WARN", content: "Vehicle has been removed" }
+          };
         }
       } else {
-        return {output: output, event: noopFor(e), message: {level: "ERROR", content: "Vehicle no longer assigned, group removed"}};
+        return {
+          output: output,
+          event: noopFor(e),
+          message: {
+            level: "ERROR",
+            content: "Vehicle no longer assigned, group removed"
+          }
+        };
       }
-        
+
       break;
     case "none":
       break;
     default:
       assertNever(eventType, "parameter");
   }
-  return {output: output, event: e};
+  return { output: output, event: e };
 };
 
-let handleRemove = (legacyInput: LegacyInput, e: event, output: Output): HandlerResponse => {
+let handleRemove = (
+  legacyInput: LegacyInput,
+  e: event,
+  output: Output
+): HandlerResponse => {
   let eventType = e[EventIndex.Type];
-  switch(eventType) {
+  switch (eventType) {
     case "exclusion":
       let driver = legacyInput.people.find(p => p.id === e[EventIndex.Target]);
-      let vehicle = legacyInput.vehicles.find(v => v.id === e[EventIndex.Value]);
-      if (!driver || !vehicle ){
-        return {output: output, event: noopFor(e), message: {level: "NOTIFY", content: "Exclusion Not Removed"}};
+      let vehicle = legacyInput.vehicles.find(
+        v => v.id === e[EventIndex.Value]
+      );
+      if (!driver || !vehicle) {
+        return {
+          output: output,
+          event: noopFor(e),
+          message: { level: "NOTIFY", content: "Exclusion Not Removed" }
+        };
       }
       output.exclusions = output.exclusions.filter(r => {
-        r.driver_id !== driver!.id || r.vehicle_id !== vehicle!.id
+        r.driver_id !== driver!.id || r.vehicle_id !== vehicle!.id;
       });
-      return {output: output, event: e};
+      return { output: output, event: e };
       break;
     case "group":
     case "groupLimit":
@@ -146,8 +222,8 @@ let handleRemove = (legacyInput: LegacyInput, e: event, output: Output): Handler
     default:
       assertNever(eventType, "parameter");
   }
-  return {output: output, event: e};
-}
+  return { output: output, event: e };
+};
 
 function handleInput(input: LegacyInput) {
   let output: Output = {
@@ -157,7 +233,7 @@ function handleInput(input: LegacyInput) {
   let response: HandlerResponse;
 
   let messages: Message[] = [];
-  
+
   let newEvents = events.map(e => {
     switch (e[EventIndex.Command]) {
       case "set":
@@ -167,14 +243,14 @@ function handleInput(input: LegacyInput) {
         response = handleRemove(input, e, output);
         break;
       case "noop":
-        response = {output: output, event: noopFor(e)};
-        break
+        response = { output: output, event: noopFor(e) };
+        break;
       default:
-        throw "Invalid command: " + e[EventIndex.Command]
+        throw "Invalid command: " + e[EventIndex.Command];
     }
     output = response.output;
-    if (response.message){
-      if (response.message.level === "ERROR"){
+    if (response.message) {
+      if (response.message.level === "ERROR") {
         let err: any = new Error("Log Replay Error");
         err.replayMessage = response.message;
         throw err;
@@ -194,7 +270,7 @@ try {
   handleInput(legacyInputInitial);
   handleInput(legacyInputMissingPersonOne);
   handleInput(legacyInputMissingPersonOneAndTwo);
-} catch(e) {
+} catch (e) {
   console.log(e.message);
   console.log(JSON.stringify(e.replayMessage));
 }
